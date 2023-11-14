62abfed1fc09d114ee60a90c5ec836d815e8a3e1
AnisBenFerhat <anisbenferhat@gmail.com>
2023-11-14 16:27:55 +0100

    feat(Billboard): Style the billboard section
    Adjusted styles for the logo and buttons in the Billboard component to
    achieve proper alignment, sizing, and hover effects.

--- src/components/Billboard/Billboard.tsx
+++ src/components/Billboard/Billboard.tsx
@@ -1,9 +1,28 @@
 import styles from './Billboard.module.css';
+import GodfatherImg from '../../assets/movies/Godfather.jpg';
+import GodfatherLogo from '../../assets/movies/Godfather-Logo.png';
+import { FaPlay } from 'react-icons/fa6';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 
 function Billboard() {
   return (
     <div className={styles.billboard}>
-      <h1>Billboard</h1>
+      <div className={styles.billboardImageContainer}>
+        <img src={GodfatherImg} alt='Godfather Part 1 screenshot of the movie' className={styles.billboardImage} />
+        <div className={styles.innerBillboard}>
+          <div className={styles.logoContainer}>
+            <img src={GodfatherLogo} alt='Logo of the Godfather movie' className={styles.logo} />
+            <div className={styles.buttonsContainer}>
+              <button className={styles.button}>
+                <FaPlay /> Play
+              </button>
+              <button className={styles.button}>
+                <IoInformationCircleOutline /> More Info
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
